c504a320e93b2334b9e131e2832665943ea07e78
Akshat Aggarwal <akshat010598@gmail.com>
2017-02-02 14:18:47 -0800

    Exercise 1 and 3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,27 +3,37 @@
 # Load the httr and jsonlite libraries for accessing data
 library(httr)
 library(jsonlite)
-
+library(dplyr)
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
 # For what years does the API have statistical data?
-GET(http://data.unhcr.org/wiki/index.php/API_Documentation.html)
-
+response <- GET("http://data.unhcr.org/api/stats/time_series_years.json") 
+body <- fromJSON(content(response, "text"))
+print(body)
 # What is the "country code" for the "Syrian Arab Republic"?
+response.country.code <- GET("http://data.unhcr.org/api/countries/list.json")
+body <- fromJSON(content(response.country.code, "text"))
+print(body)
 
+body %>%  filter(name_en == "Syrian Arab Republic") %>%  select(country_code)
 
 # How many persons of concern from Syria applied for residence in the USA in 2013?
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
-
+query.param <- list(year = 2013, country_of_origin = "SYR", country_of_residence = "USA")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json", query = query.param)
+body <- fromJSON(content(response, "text"))
 
 ## And this was only 2013...
 
 
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
+query.param <- list(population_type_code = "RF", country_of_origin = "SYR", country_of_residence = "USA")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json", query = query.param)
+body <- fromJSON(content(response, "text"))
 
 
 # Use the `plot()` function to plot the year vs. the value.
@@ -35,6 +45,10 @@ GET(http://data.unhcr.org/wiki/index.php/API_Documentation.html)
 # How many *refugees* from Syria settled in that country in all years in the data set (2000 through 2013)?
 # Is it more or less than the USA? (Hint: join the tables and add a new column!)
 # Hint: To compare the values, you'll need to convert the data (which is a string) to a number; try using `as.numeric()`
+query.param <- list(population_type_code = "RF", country_of_origin = "SYR", country_of_residence = "TUR")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json", query = query.param)
+body <- fromJSON(content(response, "text"))
+
 
 
 
